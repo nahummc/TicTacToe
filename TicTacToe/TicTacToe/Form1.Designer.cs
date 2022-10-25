3a111389f3d95e0c48fc35f11991f9bed98bf77c
Nahum <nahumcollicott@gmail.com>
2022-10-24 21:10:54 -0300

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bx1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bx2 = new System.Windows.Forms.Label();
+            this.bx3 = new System.Windows.Forms.Label();
+            this.bx4 = new System.Windows.Forms.Label();
+            this.bx5 = new System.Windows.Forms.Label();
+            this.bx6 = new System.Windows.Forms.Label();
+            this.bx7 = new System.Windows.Forms.Label();
+            this.bx8 = new System.Windows.Forms.Label();
+            this.bx9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.statelbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bx1
+            // 
+            this.bx1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx1.Location = new System.Drawing.Point(247, 181);
+            this.bx1.Name = "bx1";
+            this.bx1.Size = new System.Drawing.Size(80, 84);
+            this.bx1.TabIndex = 0;
+            this.bx1.Click += new System.EventHandler(this.bx1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(331, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tic Tac Toe";
+            // 
+            // bx2
+            // 
+            this.bx2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx2.Location = new System.Drawing.Point(382, 181);
+            this.bx2.Name = "bx2";
+            this.bx2.Size = new System.Drawing.Size(80, 84);
+            this.bx2.TabIndex = 2;
+            this.bx2.Click += new System.EventHandler(this.bx2_Click);
+            // 
+            // bx3
+            // 
+            this.bx3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx3.Location = new System.Drawing.Point(501, 181);
+            this.bx3.Name = "bx3";
+            this.bx3.Size = new System.Drawing.Size(80, 84);
+            this.bx3.TabIndex = 3;
+            this.bx3.Click += new System.EventHandler(this.bx3_Click);
+            // 
+            // bx4
+            // 
+            this.bx4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx4.Location = new System.Drawing.Point(247, 291);
+            this.bx4.Name = "bx4";
+            this.bx4.Size = new System.Drawing.Size(80, 84);
+            this.bx4.TabIndex = 4;
+            this.bx4.Click += new System.EventHandler(this.bx4_Click);
+            // 
+            // bx5
+            // 
+            this.bx5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx5.Location = new System.Drawing.Point(372, 291);
+            this.bx5.Name = "bx5";
+            this.bx5.Size = new System.Drawing.Size(80, 84);
+            this.bx5.TabIndex = 5;
+            this.bx5.Click += new System.EventHandler(this.bx5_Click);
+            // 
+            // bx6
+            // 
+            this.bx6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx6.Location = new System.Drawing.Point(501, 291);
+            this.bx6.Name = "bx6";
+            this.bx6.Size = new System.Drawing.Size(80, 84);
+            this.bx6.TabIndex = 6;
+            this.bx6.Click += new System.EventHandler(this.bx6_Click);
+            // 
+            // bx7
+            // 
+            this.bx7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx7.Location = new System.Drawing.Point(247, 406);
+            this.bx7.Name = "bx7";
+            this.bx7.Size = new System.Drawing.Size(80, 84);
+            this.bx7.TabIndex = 7;
+            this.bx7.Click += new System.EventHandler(this.bx7_Click);
+            // 
+            // bx8
+            // 
+            this.bx8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx8.Location = new System.Drawing.Point(372, 406);
+            this.bx8.Name = "bx8";
+            this.bx8.Size = new System.Drawing.Size(80, 84);
+            this.bx8.TabIndex = 8;
+            this.bx8.Click += new System.EventHandler(this.bx8_Click);
+            // 
+            // bx9
+            // 
+            this.bx9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bx9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bx9.Location = new System.Drawing.Point(501, 406);
+            this.bx9.Name = "bx9";
+            this.bx9.Size = new System.Drawing.Size(80, 84);
+            this.bx9.TabIndex = 9;
+            this.bx9.Click += new System.EventHandler(this.bx9_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(335, 129);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(165, 29);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "It is     \'s turn.";
+            // 
+            // statelbl
+            // 
+            this.statelbl.AutoSize = true;
+            this.statelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statelbl.Location = new System.Drawing.Point(385, 129);
+            this.statelbl.Name = "statelbl";
+            this.statelbl.Size = new System.Drawing.Size(31, 29);
+            this.statelbl.TabIndex = 11;
+            this.statelbl.Text = "X";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 613);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 50);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 746);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statelbl);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.bx9);
+            this.Controls.Add(this.bx8);
+            this.Controls.Add(this.bx7);
+            this.Controls.Add(this.bx6);
+            this.Controls.Add(this.bx5);
+            this.Controls.Add(this.bx4);
+            this.Controls.Add(this.bx3);
+            this.Controls.Add(this.bx2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bx1);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label bx1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label bx2;
+        private System.Windows.Forms.Label bx3;
+        private System.Windows.Forms.Label bx4;
+        private System.Windows.Forms.Label bx5;
+        private System.Windows.Forms.Label bx6;
+        private System.Windows.Forms.Label bx7;
+        private System.Windows.Forms.Label bx8;
+        private System.Windows.Forms.Label bx9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label statelbl;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public void check_win()
+        //{
+        //    //handle horizontal cases
+        //    if (bx1.Text == bx2.Text && bx2.Text == bx3.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    //2
+        //    else if (bx4.Text == bx5.Text && bx5.Text == bx6.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    //3
+        //    else if (bx7.Text == bx8.Text && bx8.Text == bx9.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    // handle vertical cases 1
+        //    else if (bx1.Text == bx4.Text && bx4.Text == bx7.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    //2
+        //    else if (bx2.Text == bx5.Text && bx5.Text == bx8.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    //3
+        //    else if (bx3.Text == bx6.Text && bx6.Text == bx9.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    //diagonal 1
+        //    else if (bx1.Text == bx5.Text && bx5.Text == bx9.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    //2
+        //    else if (bx3.Text == bx5.Text && bx5.Text == bx7.Text)
+        //    {
+        //        string winner = bx2.Text;
+        //        MessageBox.Show(winner + " wins!");
+        //    }
+        //    else 
+        //    {
+        //        Console.WriteLine("hello");
+        //    }
+        //}
+
+        public void check_win()
+
+
+            // TODO
+            // FIX THE WAY THE PROJECT READS THE WINNER
+
+
+        {
+            //handle horizontal cases
+            if (bx1.Text != "" && bx1.Text == bx2.Text && bx2.Text == bx3.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            //2
+            else if (bx4.Text != "" && bx4.Text == bx5.Text && bx5.Text == bx6.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            //3
+            else if (bx7.Text != "" && bx7.Text == bx8.Text && bx8.Text == bx9.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            // handle vertical cases 1
+            else if (bx1.Text != "" && bx1.Text == bx4.Text && bx4.Text == bx7.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            //2
+            else if (bx2.Text != "" && bx2.Text == bx5.Text && bx5.Text == bx8.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            //3
+            else if (bx3.Text != "" && bx3.Text == bx6.Text && bx6.Text == bx9.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            //diagonal 1
+            else if (bx4.Text != "" && bx1.Text == bx5.Text && bx5.Text == bx9.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            //2
+            else if (bx3.Text != "" && bx3.Text == bx5.Text && bx5.Text == bx7.Text)
+            {
+                string winner = bx2.Text;
+                MessageBox.Show("Game Over");
+            }
+            else
+            {
+                Console.WriteLine("hello");
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bx1.Text = "";
+            bx2.Text = "";
+            bx3.Text = "";
+            bx4.Text = "";
+            bx5.Text = "";
+            bx6.Text = "";
+            bx7.Text = "";
+            bx8.Text = "";
+            bx9.Text = "";
+        }
+
+        private void bx1_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx1.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx1.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+        }
+        private void bx2_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx2.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx2.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+
+        }
+
+        private void bx3_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx3.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx3.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+
+        }
+
+        private void bx4_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx4.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx4.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+        }
+
+        private void bx5_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx5.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx5.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+        }
+
+        private void bx6_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx6.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx6.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+        }
+
+        private void bx7_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx7.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx7.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+        }
+
+        private void bx8_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx8.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx8.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+        }
+
+        private void bx9_Click(object sender, EventArgs e)
+        {
+            if (statelbl.Text == "X")
+            {
+                bx9.Text = "X";
+                statelbl.Text = "O";
+            }
+            else if (statelbl.Text == "O")
+            {
+                bx9.Text = "O";
+                statelbl.Text = "X";
+            }
+            check_win();
+        }
+    }
+}
